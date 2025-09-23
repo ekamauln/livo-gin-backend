f09745e82f45e14c9f41299525473ea946b92a76
ekamauln <eka.mauln@gmail.com>
2025-09-23 11:06:00 +0700

    adding search by order id or tracking in mobile routes

--- routes/order_mobile_routes.go
+++ routes/order_mobile_routes.go
@@ -16,16 +16,19 @@ func SetupOrderMobileRoutes(api *gin.RouterGroup, cfg *config.Config, orderMobil
 	{
 		// Get all orders ready to pick
 		mobileOrders.GET("", orderMobileController.GetOrders)
-		
+
 		// Get my ongoing picking orders (status: "picking process")
 		mobileOrders.GET("/my-picking", orderMobileController.GetMyPickingOrders)
-		
+
+		// Search orders by order ID or tracking number
+		mobileOrders.GET("/search", orderMobileController.SearchOrders)
+
 		// Pick an order (change status to "picking process")
 		mobileOrders.PUT("/:id/pick", orderMobileController.PickingOrder)
-		
+
 		// Get order details with product info (location, barcode)
 		mobileOrders.GET("/:id", orderMobileController.GetOrder)
-		
+
 		// Complete picking process (change status to "picking complete")
 		mobileOrders.PUT("/:id/complete", orderMobileController.CompletePickingOrder)
 	}
